a146b88241d25539fbaf7cb7d4cb967d042ef188
kadan-develop <daniel.kacanski@gmail.com>
2025-01-10 00:20:11 +0100

    styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./src/**/*.{html,js}"];
 export const theme = {
-  extend: {},
+  extend: {
+    fontFamily: {
+      display: "DM Serif Display, serif",
+      karla: "Karla, sans-serif",
+    },
+  },
 };
 export const plugins = [];
